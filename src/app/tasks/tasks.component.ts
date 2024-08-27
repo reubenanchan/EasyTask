4d72300573419a585b1c6ef223751581e6b712b2
reubenanchan <71098011+reubenanchan@users.noreply.github.com>
2024-08-27 14:36:24 -0400

    Initial commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: false,
+  //imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
